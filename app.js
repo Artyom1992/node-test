26b752c818e211d78aa87fb00428b4dce3df256c
alazukin <alazukin@samsonpost.ru>
2017-10-12 17:56:21 +0300

    Изучение работы и запуска веб-сервера Необходимо повторить про EventEmitter

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var server = require('server');
-var util = require('util');
 
-/* МОДУЛЬ(ОБЪЕКТ) UTIL */
+server.run();
+
+/*------------- МОДУЛЬ(ОБЪЕКТ) UTIL -------------*/
+var util = require('util');
 
 /* 1. красиво выведт любой объект, можно и просто так: console.log(obj); Также имеются доп. параметры */
 var obj = { a: 5, b: 6 }
@@ -10,7 +12,6 @@ console.log(util.inspect(obj/*, {...} */));
 
 /* 2. получает строку, а дальше: 1) вместо %s, будет выведена строка "string", 2) вместо %d, будет число 123, 3) вместо %j, будет объект в формате JSON*/
 console.log(util.format("My %s %d %j", "string", 123, {test: "obj"}));
-
 
 /* 3. создает наследование, внутри это метода лежит Object.create()*/
 function Animal(name) {
@@ -33,9 +34,8 @@ rabbit.walk();
 rabbit.jump();
 
 
-server.run();
+/*------------- МОДУЛЬ(ОБЪЕКТ) CONSOLE -------------*/
 
-/* МОДУЛЬ(ОБЪЕКТ) CONSOLE */
 /* 1. Использует util.format, util.inspect */
 /* 2. console - это глобальная переменная, её не надо подключать(require) */
 /* 3. Имеет всего два метода: console.log() и console.error() */
@@ -43,8 +43,7 @@ console.log("Log"); // = info (out)
 console.error("Error"); // = warn (err)
 
 
-/* ОБЪЕКТ Error */
-
+/*------------- ОБЪЕКТ ERROR -------------*/
 var phrases = {
 	"Hello": "Привет",
 	"world": "мир"
@@ -53,6 +52,7 @@ var phrases = {
 
 function PhraseError(message) {
 	this.message = message;
+	Error.captureStackTrace(this, PhraseError);
 }
 util.inherits(PhraseError, Error);
 PhraseError.prototype.name = 'PhraseError';
@@ -60,6 +60,7 @@ PhraseError.prototype.name = 'PhraseError';
 function HttpError(status, message) {
 	this.status = status;
 	this.message = message;
+	Error.captureStackTrace(this, HttpError)
 }
 util.inherits(HttpError, Error);
 HttpError.prototype.name = 'HttpError';
@@ -77,8 +78,38 @@ function makePage(url) {
 	if (url != 'index.html') {
 		throw new HttpError(404, "Нет такой страницы");
 	}
-	return util.format("%s, %s!", getPhrase("Hell"), getPhrase("world"));
+	return util.format("%s, %s!", getPhrase("Hello"), getPhrase("world"));
 }
 
-var page = makePage('index.ht');
-console.log(page);
+try {
+	var page = makePage('index.html');
+	console.log(page);	
+} catch (e) {
+	if (e instanceof HttpError) {
+		console.error(e.status, e.message);
+	} else {
+		console.error("Ошибка %s\n сообщение: %s\n стек: %s", e.name, e.message, e.stack);
+	}
+}
+
+
+/*------------- ОБЪЕКТ EVENTEMITTER -------------*/  /* ? ЕЩЁ РАЗ РАЗОБРАТЬ */
+/* Основной объект, реализующий работу с событиями в node.js, остальные встроенные объекты(работающие с событиями) наследуются от него */
+var EventEmitter = require('events').EventEmitter;
+
+var server = new EventEmitter;
+
+/* метод подписка "on" */
+server.on('request', function(request) {
+	request.approved = true;
+});
+
+server.on('request', function(request) {
+	console.log(request);
+});
+
+/* генерит событие и передает данные(эти данные попадают в функцию обработчик) "emit" */
+server.emit('request', {from: "Клиент"});
+server.emit('request', {from: "Ещё Клиент"});
+
+console.log(util.inspect(server.listeners('request')));
--- server.js
+++ server.js
@@ -22,15 +22,35 @@ if (module.parent) {
 
 
 // Запуск сервера
-var http = require('http');
+ var http = require('http');
 var url = require('url');
-var server = new http.Server(function(req, res) {
-	console.log('Метод: ' + req.method, '\n' + 'Url: ' + req.url);
 
+/* var server = new http.Server(); // Сервер является EventEmmiter
+server.listen(1337, '127.0.0.1');
+
+var counter = 0;
+server.on('request', function(req, res) {
+	// req - входящий запрос, получает то, что присылает бразуер, включая url, пришедшего запроса
+	// response -  объект ответа, обязателен res.end();
+	res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+	res.end("Привет, мир!" + ++counter);
+}); */
+
+ var server = new http.Server(function(req, res) {
+	console.log(req.headers);
+
+/* 	res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+
+	console.log('Метод: ' + req.method, '\n' + 'Url: ' + req.url);
 	var urlParsed = url.parse(req.url, true);
 	console.log(urlParsed);
-/* 
-	if (urlParsed.pathname = '/echo' && ) */
+
+	if (urlParsed.pathname = '/' && urlParsed.query.message) {
+		res.end(urlParsed.query.message)
+	} else {
+		res.statusCode = 404;
+		res.end("Page not found");
+	} */
 });
 
 server.listen(1337, '127.0.0.1');
