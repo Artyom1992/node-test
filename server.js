d5de4b1ed8e27f3a922be8a27bd5d251503c3390
alazukin <alazukin@samsonpost.ru>
2017-10-13 18:25:06 +0300

    Продолжение изучения работы и запуска веб-сервера Необходимо повторить про EventEmitter

--- server.js
+++ server.js
@@ -21,8 +21,9 @@ if (module.parent) {
 }
 
 
+var util = require('util');
 // Запуск сервера
- var http = require('http');
+var http = require('http2');
 var url = require('url');
 
 /* var server = new http.Server(); // Сервер является EventEmmiter
@@ -30,27 +31,28 @@ server.listen(1337, '127.0.0.1');
 
 var counter = 0;
 server.on('request', function(req, res) {
-	// req - входящий запрос, получает то, что присылает бразуер, включая url, пришедшего запроса
+	// req - входящий запрос, сервер получает то, что присылает бразуер, включая url, пришедшего запроса
 	// response -  объект ответа, обязателен res.end();
 	res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
 	res.end("Привет, мир!" + ++counter);
 }); */
 
  var server = new http.Server(function(req, res) {
-	console.log(req.headers);
+	console.log(util.format("%s: %j", "Ответы(то, что присылает) бразуера", req.headers));
 
-/* 	res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+/* 	res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'}); */
 
 	console.log('Метод: ' + req.method, '\n' + 'Url: ' + req.url);
 	var urlParsed = url.parse(req.url, true);
 	console.log(urlParsed);
 
 	if (urlParsed.pathname = '/' && urlParsed.query.message) {
-		res.end(urlParsed.query.message)
+		res.setHeader('Cache-control', 'no-cache');
+		res.end(urlParsed.query.message);
 	} else {
 		res.statusCode = 404;
 		res.end("Page not found");
-	} */
+	}
 });
 
 server.listen(1337, '127.0.0.1');
